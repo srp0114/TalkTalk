57926296294d1ecdd982b01566ae8eb39f4f3f6f
babo0121 <ydbwlsjki60@gmail.com>
2022-11-10 21:55:28 +0900

    로고 부탁 및 패널 생성

--- TalkTalkClient/src/client/TalkTalkClientMain.java
+++ TalkTalkClient/src/client/TalkTalkClientMain.java
@@ -2,9 +2,12 @@ package client;
 
 import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -14,7 +17,9 @@ import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
 public class TalkTalkClientMain extends JFrame {
-
+	private Image logoImg = Toolkit.getDefaultToolkit().getImage("src/talkLogo.png");
+	private ImageIcon talkLogo;
+	private JLabel lblTalkLogo;
 	private JPanel contentPane;
 	private JTextField txtUserName;
 	
@@ -37,25 +42,32 @@ public class TalkTalkClientMain extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 380, 600);
 		contentPane = new JPanel();
-		contentPane.setBackground(new Color(249,229,93));
+		contentPane.setBackground(new Color(254,229,0));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		// 이미지 크기 조정
+		logoImg = logoImg.getScaledInstance(100,  100,  Image.SCALE_DEFAULT);
+		talkLogo = new ImageIcon(logoImg);
+		lblTalkLogo = new JLabel(talkLogo);
+		lblTalkLogo.setBounds(106, 70, 150, 130);
+		contentPane.add(lblTalkLogo);
 		
-		JLabel lblUserName = new JLabel("User Name");
-		lblUserName.setBounds(80, 200, 100, 38);
+		
+		JLabel lblUserName = new JLabel("이름");
+		lblUserName.setBounds(80, 220, 100, 38);
 		contentPane.add(lblUserName);
 		
 		txtUserName = new JTextField();
 		txtUserName.setHorizontalAlignment(SwingConstants.CENTER);
-		txtUserName.setBounds(80, 230, 205, 38);
+		txtUserName.setBounds(80, 250, 205, 38);
 		contentPane.add(txtUserName);
 		txtUserName.setColumns(10);
 		
 		JButton btnLogin = new JButton("로그인");
-		btnLogin.setForeground(new Color(245, 245, 245));
+		btnLogin.setForeground(new Color(255, 255, 255));
 		btnLogin.setBounds(80, 300, 205, 38);
-		btnLogin.setBackground(new Color(66, 45, 45));
+		btnLogin.setBackground(new Color(66, 54, 48));
 		contentPane.add(btnLogin);
 		Myaction action = new Myaction();
 		btnLogin.addActionListener(action);
--- TalkTalkClient/src/client/TalkTalkClientView.java
+++ TalkTalkClient/src/client/TalkTalkClientView.java
@@ -1,12 +1,18 @@
 package client;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JSplitPane;
 import javax.swing.border.EmptyBorder;
 
 public class TalkTalkClientView extends JFrame{
 	private JPanel contentPane;
 	private String UserName;
+	
+	private MenuPanel menuPanel;  // 메뉴 패널
+	private FriendPanel friendPanel;  // 친구창 패널
 
 	public TalkTalkClientView(String username, String ip_addr, String port_no) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -15,6 +21,21 @@ public class TalkTalkClientView extends JFrame{
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		setResizable(false);
+		setVisible(true);
+		
+		splitPane();
+		this.repaint();
+	}
+	
+	public void splitPane() {
+		JSplitPane hPane = new JSplitPane();  
+		getContentPane().add(hPane, BorderLayout.CENTER);
+		
+		hPane.setOrientation(JSplitPane.HORIZONTAL_SPLIT);
+		hPane.setDividerLocation(30);
+		hPane.setEnabled(false);
+		hPane.setLeftComponent(menuPanel);
 		
 	}
 }
