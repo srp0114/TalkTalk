0663268a526cac630e8298182e1eb4c5829596b0
babo0121 <ydbwlsjki60@gmail.com>
2022-11-21 17:03:24 +0900

    아이콘 부착

--- TalkTalkClient/src/ChatListPanel.java
+++ TalkTalkClient/src/ChatListPanel.java
@@ -1,6 +1,10 @@
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.Image;
+import java.awt.Toolkit;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -10,7 +14,9 @@ public class ChatListPanel extends JPanel{
 	
 	private JLabel lblChating;
 	
-	
+	private Image addChatroomIconImg = Toolkit.getDefaultToolkit().getImage("src/new-message.png");
+	private ImageIcon addChatroomIcon;
+	private JButton btnAddChatroom;
 	
 	
 	public ChatListPanel(TalkTalkClientView clientView, ChatMsg chatMsg) {
@@ -29,5 +35,16 @@ public class ChatListPanel extends JPanel{
 		lblChating.setFont(new Font("맑은 고딕", Font.BOLD, 18));
 		lblChating.setBounds(23, 20, 50, 50);
 		this.add(lblChating);
+		
+		addChatroomIconImg = addChatroomIconImg.getScaledInstance(25, 25,  Image.SCALE_SMOOTH);
+		addChatroomIcon = new ImageIcon(addChatroomIconImg);
+		btnAddChatroom = new JButton(addChatroomIcon);
+		btnAddChatroom.setBorderPainted(false);
+		btnAddChatroom.setFocusPainted(false);
+		btnAddChatroom.setContentAreaFilled(false);
+		btnAddChatroom.setOpaque(false);
+		btnAddChatroom.setBounds(267, 32, 25, 25);
+		this.add(btnAddChatroom);
+		
 	}
 }
